f6fa04b58bfd151753f672d6564448607c319c9a
LOHAN2000 <anghelovy@outlook.com>
2026-02-21 15:31:33 -0500

    modificando sidebar y eliminando navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,10 @@
 "use client"
-import { useAuthStore } from '@/store/useAuthStore';
-import { LogOut } from 'lucide-react';
-import { useEffect, useState } from 'react';
 
 export const Navbar = () => {
   
-  const { user, logout } = useAuthStore();
-  const [ isMounted, setIsMounted ] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setIsMounted(true);
-  }, [])
-
   return (
     <div className='flex justify-end items-center w-full mb-7 px-4 pt-4'>
-      {isMounted && user ? (
-        <div className='flex items-center gap-4'>
-          <div className='flex flex-col text-right'>
-            <span className='text-sm font-bold text-slate-800'>{user?.unique_name}</span>
-            <span className='text-xs font-medium text-sky-400'>{user?.role}</span>
-          </div>
-          <div className='w-10 h-10 bg-red-100 text-red-600 rounded-full flex items-center justify-center font-bold'>
-            {(user?.unique_name)?.charAt(0).toUpperCase() + "" + (user?.LastName)?.charAt(0).toUpperCase()}
-          </div>
-          <button onClick={logout} className='p-2 text-slate-400 hover:bg-red-50 hover:text-red-500 rounded-xl transition-all ml-2'>
-            <LogOut className='w-5 h-5'/>
-          </button>
-        </div>
-      ) : (
-        <div className="flex items-center gap-3 animate-pulse">
-          <div className="w-24 h-4 bg-slate-200 rounded"></div>
-          <div className="w-10 h-10 bg-slate-200 rounded-full"></div>
-        </div>
-      )}
+      
     </div>
   )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,60 +1,93 @@
 "use client"
 import { useGlobalState } from '@/store'
-import { DollarSign, LayoutDashboard, Menu, Package, ShoppingCart } from 'lucide-react'
+import { DollarSign, LayoutDashboard, LogOut, Menu, Package, ShoppingCart, LucideIcon } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
+import { useAuthStore } from '@/store/useAuthStore'
+
+interface NavItem {
+  href: string;
+  label: string;
+  icon: LucideIcon;
+}
+
+const navItems: NavItem[] = [
+  { href: "/", label: "Dashboard", icon: LayoutDashboard },
+  { href: "/pos", label: "Venta", icon: DollarSign },
+  { href: "/products", label: "Productos", icon: ShoppingCart },
+  { href: "/inventory", label: "Inventario", icon: Package },
+];
 
 export const SideBar = () => {
 
-  const { isSideBarCollapsed, setIsSideBarCollapsed } = useGlobalState();
+  const { user, logout } = useAuthStore();
+  const [ isMounted, setIsMounted ] = useState(false);
 
-  const sideBarClassNames = `fixed flex flex-col bg-white overflow-hidden shadow-md z-40 h-full transition-all duration-300 ${isSideBarCollapsed ? "w-16" : "w-64"}`
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setIsMounted(true);
+  }, [])
+
+  const { isSideBarCollapsed, setIsSideBarCollapsed } = useGlobalState();
+  const pathName = usePathname();
+
+  const sideBarClassNames = `fixed flex flex-col bg-white overflow-y-auto overflow-x-hidden shadow-lg z-40 h-full transition-all duration-300 ${isSideBarCollapsed ? "w-20" : "w-64"}`
 
   return (
     <div className={sideBarClassNames}>
-      <div className={`flex items-center justify-between w-full ${isSideBarCollapsed ? "px-5.5 pt-5" : "px-8 pt-2"}`}>
+      <div className={`flex items-center ${isSideBarCollapsed ? "justify-center h-20" : "justify-center h-30"} w-full  px-4 border-b border-slate-200`}>
         {
           !isSideBarCollapsed && (
             <Link href={"/"}>
-              <div className='flex items-center justify-center overflow-hidden transition-all'>
-                <Image src={"/logo-minimarket-daniela.jpeg"} alt="Logo Minimarket Daniela" width={200} height={80} loading="eager" priority className="scale-230 transition-all"/>
+              <div className='flex items-center justify-center transition-all'>
+                <Image src={"/logo-minimarket-daniela.jpg"} alt="Logo Minimarket Daniela" width={110} height={64} style={{ objectFit: 'contain' }} loading="eager" priority/>
               </div>
             </Link>
           )
         }
-        <button className='hover:bg-gray-100 rounded-full' onClick={() => setIsSideBarCollapsed(!isSideBarCollapsed)}>
-          <Menu size={isSideBarCollapsed ? 20 : 25}/>
+        <button className={`p-2 hover:bg-red-50 text-slate-600 rounded-lg ${!isSideBarCollapsed ? 'absolute right-2 top-2' : ''}`} onClick={() => setIsSideBarCollapsed(!isSideBarCollapsed)}>
+          <Menu size={20}/>
         </button>
-        
       </div>
 
       <nav className="mt-8 flex flex-col gap-2 px-2">
-        <Link href="/" className={`flex items-center rounded-lg bg-blue-50 py-3 text-blue-700 transition-all duration-300 ${isSideBarCollapsed ? "gap-0 justify-center px-2" : "gap-3 px-4"}`}>
-          <LayoutDashboard size={20} />
-          {!isSideBarCollapsed && <span className="font-medium">Dashboard</span>}
-        </Link>
-        
-        <Link href="/pos" className={`flex items-center rounded-lg py-3 text-gray-500 hover:bg-gray-100 transition-all duration-300 ${isSideBarCollapsed ? "gap-0 justify-center px-2" : "gap-3 px-4"}`}>
-          <DollarSign size={20} />
-          {!isSideBarCollapsed && <span className="font-medium">Venta</span>}
-        </Link>
-
-        <Link href="/inventory" className={`flex items-center rounded-lg py-3 text-gray-500 hover:bg-gray-100 transition-all duration-300 ${isSideBarCollapsed ? "gap-0 justify-center px-2" : "gap-3 px-4"}`}>
-          <Package size={20} />
-          {!isSideBarCollapsed && <span className="font-medium">Inventario</span>}
-        </Link>
-
-        <Link href="/products" className={`flex items-center rounded-lg py-3 text-gray-500 hover:bg-gray-100 transition-all duration-300 ${isSideBarCollapsed ? "gap-0 justify-center px-2" : "gap-3 px-4"}`}>
-          <ShoppingCart size={20} />
-          {!isSideBarCollapsed && <span className="font-medium">Productos</span>}
-        </Link>
-
+        {navItems.map((item) => (
+          <Link key={item.href} href={item.href} className={`flex items-center rounded-lg py-3 transition-all duration-300 ${pathName === item.href ? "bg-red-600 text-white shadow-lg shadow-red-500/30" : "text-slate-500 hover:bg-red-50 hover:text-red-600"} ${isSideBarCollapsed ? "justify-center" : "gap-3 px-4"}`}>
+            <item.icon size={20} />
+            {!isSideBarCollapsed && <span className="font-medium">{item.label}</span>}
+          </Link>
+        ))}
       </nav>
 
-      {/* 3. Footer del menú */}
-      <div className="absolute bottom-0 w-full p-4 text-center text-xs text-gray-400">
-        {!isSideBarCollapsed && <p>&copy; 2026 Minimarket Daniela</p>}
+      <div className="mt-auto p-2">
+        {isMounted && user ? (
+          <div className={`flex items-center gap-3 ${isSideBarCollapsed ? 'justify-center' : 'p-2'}`}>
+            <div className='w-10 h-10 bg-red-100 text-red-600 rounded-full flex items-center justify-center font-bold shrink-0'>
+              {(user?.unique_name)?.charAt(0).toUpperCase() + "" + (user?.LastName)?.charAt(0).toUpperCase()}
+            </div>
+            {!isSideBarCollapsed && (
+              <>
+                <div className='flex flex-col text-start overflow-hidden'>
+                  <span className='text-sm font-bold text-slate-800 truncate'>{user?.unique_name}</span>
+                  <span className='text-xs font-bold text-red-500'>{user?.role}</span>
+                </div>
+                <button onClick={logout} className='p-2 text-slate-500 hover:bg-red-100 hover:text-red-600 rounded-lg transition-all ml-auto'>
+                  <LogOut className='w-5 h-5'/>
+                </button>
+              </>
+            )}
+          </div>
+        ) : (
+          <div className="flex items-center justify-center gap-3 animate-pulse p-2">
+            <div className="w-10 h-10 bg-slate-200 rounded-full"></div>
+            {!isSideBarCollapsed && <div className="w-24 h-4 bg-slate-200 rounded"></div>}
+          </div>
+        )}
+        <div className={`w-full pt-4 text-center text-xs text-slate-400 border-t border-slate-200/80 mt-2 ${isSideBarCollapsed ? "hidden" : "block"}`}>
+          <p>&copy; 2026 Minimarket Daniela</p>
+        </div>
       </div>
 
     </div>
